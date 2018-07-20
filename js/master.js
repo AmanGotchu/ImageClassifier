8bf425441c8c45c85f31b6ee8c4d48bea8857a51
amangotchu <aman.gottu@gmail.com>
2018-07-20 11:08:19 -0500

    Fixed AJAX request. Console.logs answer

--- js/master.js
+++ js/master.js
@@ -2,18 +2,19 @@
 
 function clickMe(){
   console.log("Clicked");
-  $.ajax({
-      url: 'https://cnnflaskapi.herokuapp.com/classify',
-      type: 'post',
-      data: {
-          imageURL: 'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg'
-      },
-      headers: {
-          content-type: 'application/json' //for object property name, use quoted notation shown in second
-      },
-      dataType: 'json',
-      success: function (data) {
-          console.info(data);
-      }
+
+  $.get( "https://cnnflaskapi.herokuapp.com/hello", function( data ) {
+    console.log(data);
   });
+
+  $.ajax({
+    url:'https://cnnflaskapi.herokuapp.com/classify',
+    type:"POST",
+    data: JSON.stringify({"imageURL": $('#urlinput').val()}),
+    contentType:"application/json",
+    success: function(response){
+      console.log(response);
+    }
+  })
+
 }
