bd0d2db766bc853bd0cd17430e2d6a80d5fb0127
amangotchu <aman.gottu@gmail.com>
2018-07-20 10:30:16 -0500

    Added https instead of http

--- js/master.js
+++ js/master.js
@@ -2,7 +2,7 @@
 
 function clickMe(){
   console.log("Clicked");
-  $.post('http://cnnflaskapi.herokuapp.com/classify',   // url
+  $.post('https://cnnflaskapi.herokuapp.com/classify',   // url
        { imageURL: 'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg' }, // data to be submit
        function(data, status, jqXHR) {// success callback
                 console.log(status);
